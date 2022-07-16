8111fd993517bef4b73b09e4cac936996bb85985
Laviz-pand <pandeylaviz@gmail.com>
2022-07-16 16:57:02 +0545

    About

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+
+export default function About() {
+  return (
+    <div className='about-container'>
+        <h1>About</h1>
+        <div className='about-box'>
+
+        </div>
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Homepage from './Homepage';
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './Navbar'
+import About from './About';
 
 
 function App() {
   return (
    <div>
     <Navbar/>
+      <Homepage/>
+      <About/>
+ 
 
-    <Routes>
-      <Route path='/' element={<Homepage/>} />
-    </Routes>
+
    </div>
     
   );
