52c2dcdf0cde78f3dc1203948802204bee8c6f2f
Laviz-pand <pandeylaviz@gmail.com>
2022-09-07 14:42:08 +0545

    New Start

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import './homepage.css'
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar/>
+      asd
+    </div>
+  )
+}
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../styles/footer.css'
-export default function Footer() {
-  return (
-    <div className='footer'>
-        <h1>Copyright @LavizPandey</h1>
-    </div>
-  )
-}
