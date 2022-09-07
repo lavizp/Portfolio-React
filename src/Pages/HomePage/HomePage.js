6be8d3cbe460d432010f621cc09cc53ab9af9072
Laviz-pand <pandeylaviz@gmail.com>
2022-09-07 15:38:57 +0545

    Intro Update

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,7 +5,26 @@ export default function HomePage() {
   return (
     <div>
       <Navbar/>
-      asd
+      <div className='home-intro-container'>
+        <div className='home-intro-left'>
+          <div className='intro-text'>
+            <h1>Hi! I am</h1>
+            <h1>Laviz Pandey</h1>
+            <h3>Professional Software Developer  with<br/>over <mark className='gradient'>2 years</mark> of development experience.</h3>
+            <div className='intro-buttons'>
+              <button className='orange'>Projects</button>
+              <button className='grey'>Contact</button>
+
+            </div>
+
+          </div>
+          <div className='intro-connect'>
+            asd
+          </div>
+        </div>
+        <div className='home-intro-right'></div>
+
+      </div>
     </div>
   )
 }
