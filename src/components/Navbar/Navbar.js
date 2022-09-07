fb127bbf91208bb2b4e5a893bbf0a91a3ff48fef
Laviz-pand <pandeylaviz@gmail.com>
2022-09-07 15:02:39 +0545

    Navbar Design

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,20 @@ import "./navbar.css"
 export default function Navbar() {
   return (
     <div className='nav-container'>
-        asd
+        <div className='nav-logo'>
+            <h1>Laviz</h1>
+        </div>
+        <div className='nav-list-container'>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Works</a></li>
+                <li><a>About</a></li>
+                <li><a>Experience</a></li>
+            </ul>
+        </div>
+        <div className='nav-resume'>
+            <button> Resume </button>
+        </div>
     </div>
   )
 }
