b80a8ba10b26c3e55d8decfddabdf876bdb6cf33
Laviz-pand <pandeylaviz@gmail.com>
2022-09-08 14:03:08 +0545

    Update

---
+++ src/components/ProjectsHome/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './projects.css'
+
+export default function Projects() {
+  return (
+    <div className='home-project-container'>
+        <div className='projects-intro'>
+            <h1 id="moveOnHover">Title ho</h1>
+            <h2 id="moveOnHover">E-Commerse App</h2>
+
+        </div>
+
+        <a href=''>View Project</a>
+    </div>
+  )
+}
