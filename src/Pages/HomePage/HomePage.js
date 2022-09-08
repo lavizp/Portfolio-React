6f8f11be968afa9202cf1a22c3a2aedb4d4bb3ac
Laviz-pand <pandeylaviz@gmail.com>
2022-09-08 12:40:40 +0545

    Update

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {FaGithub,FaLinkedin,FaTwitterSquare} from 'react-icons/fa';
 
 import Navbar from '../../components/Navbar/Navbar'
+import Projects from '../../components/ProjectsHome/Projects';
 import './homepage.css'
 import Laviz from '../../assets/Laviz.png'
 export default function HomePage() {
@@ -11,7 +12,7 @@ export default function HomePage() {
       <div className='home-intro-container'>
         <div className='home-intro-left'>
           <div className="intro-text">
-            <h1>Hi! I am</h1>
+            <h1>HI! I am</h1>
             <h1>Laviz Pandey</h1>
             <h3>Professional Software Developer  with<br/>over <mark className='gradient'>2 years</mark> of development experience.</h3>
             <div className='intro-buttons'>
@@ -40,6 +41,9 @@ export default function HomePage() {
       </div>
       <div className='home-projects-container'>
         <h1>Projects</h1>
+        <div className='projects-list'>
+          <Projects/>
+        </div>
       </div>
     </div>
   )
