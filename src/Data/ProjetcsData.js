8e4ed6c10c28c6d04d02912099f38c5703986d06
Laviz-pand <pandeylaviz@gmail.com>
2022-09-11 21:35:01 +0545

    Update

--- src/Data/ProjetcsData.js
+++ src/Data/ProjetcsData.js
@@ -9,7 +9,7 @@ export const projectsData =
         "id":0,
         "title": "BooksGasm Nepal",
         "shortdescription": "E-Commerse Website",
-        "description": "lorkjasdkjas dkasbdjkasbd asdb iajksbda sd iab da sdiasbdjkas dkjas ",
+        "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  ",
         "image":Resturant
     },
     {
--- src/Pages/SingleProjectPage/SingleProjectPage.js
+++ src/Pages/SingleProjectPage/SingleProjectPage.js
@@ -2,22 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './singleproject.css'
 import {projectsData} from '../../Data/ProjetcsData'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
 
 export default function SingleProjectPage() {
     const {id} = useParams();
-    const[data,setData] = useState([]);
-    const[project,setProject] = useState([]);
+    const[project,setProject] = useState();
 
     useEffect(()=>{
-      setData(
-        projectsData.filter((data)=> data.id == id)
-      );
+      const tempData = projectsData.filter((data)=> data.id == id)
+      setProject(tempData[0]);
     },[])
     
   return (
     <div>
-        asd
-        data is {data[0].title}
+      <Navbar/>
+      <h1 className='main-title-projects'>{project?.title}</h1>
+      <div className='dark-background'>
+          <img src={project?.image} alt='WebsiteImage' className='full-page-image'/>
+          <div className='singleproj-data-container'>
+            <div className='technical-details'>
+              <h1>Technical Details</h1>
+              <p>
+              Tech Used: React, SCSS,Firebase<br/>
+              </p>
+              <p>
+              Platform: Web<br/>
+              </p>
+              <p>
+              Catogery: E-Commerce Application
+              </p>
+              <div className='tech-details-buttons'>
+                <button id = 'growOnHover' className='tech-button blackbutton'>Code</button>
+                <button id = 'growOnHover'className='tech-button orange'>Launch</button>
+
+              </div>
+              </div>
+            <div className='line'></div>
+            <div className='development-story'>
+              <h1>Development Story</h1>
+              <p>
+                {project?.description}
+              </p>
+            </div>
+          </div>
+      </div>
+      <Footer/>
+
     </div>
   )
 }
