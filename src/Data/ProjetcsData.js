d6932d5ad3ff4939eb5721a9ac387e14242de166
Laviz-pand <pandeylaviz@gmail.com>
2022-09-12 07:54:18 +0545

    Update

--- src/Data/ProjetcsData.js
+++ src/Data/ProjetcsData.js
@@ -1,6 +1,6 @@
 import Resturant from '../assets/Resturant.png'
 import CubeSurfers from '../assets/CubeSurfers.png'
-
+import Booksgasm from '../assets/Booksgasm.png'
 
 
 export const projectsData =
@@ -10,20 +10,26 @@ export const projectsData =
         "title": "BooksGasm Nepal",
         "shortdescription": "E-Commerse Website",
         "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  ",
-        "image":Resturant
+        "image":Booksgasm,
+        "github":"https://github.com/lavizp/BooksgasmNepalWebsite",
+        "link":""
     },
     {
         "id":1,
         "title": "Cube-Surfers",
         "shortdescription": "E-Commerse Website",
         "description": "lorkjasdkjas dkasbdjkasbd asdb iajksbda sd iab da sdiasbdjkas dkjas ",
-        "image":CubeSurfers
+        "image":CubeSurfers,
+        "github":"",
+        "link":"https://play.google.com/store/apps/details?id=com.gamedevcluub.moneyy&hl=en&gl=US"
     },
     {
         "id":2,
         "title": "Resturante Indian",
         "shortdescription": "Resturant Landing Page",
         "description": "lorkjasdkjas dkasbdjkasbd asdb iajksbda sd iab da sdiasbdjkas dkjas ",
-        "image":Resturant
+        "image":Resturant,
+        "github":"",
+        "link":""
     }
 ]
