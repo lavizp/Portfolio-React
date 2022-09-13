cf44439f1e7e479745722e8ce44a7f449d5ec58c
Laviz-pand <pandeylaviz@gmail.com>
2022-09-13 15:24:36 +0545

    Update

---
+++ src/Pages/ExperiencePage/ExperiencePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './experience.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+export default function ExperiencePage() {
+  return (
+    <div>
+        <Navbar/>
+            ExperiencePage
+        <Footer/>
+    </div>
+  )
+}
