aef9df018e8c019074e2c8012057c861c974c913
Laviz-pand <pandeylaviz@gmail.com>
2022-09-15 18:47:37 +0545

    Navbar Updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
 import { Link } from 'react-router-dom'
 export default function Navbar() {
+    const openNav = () =>{
+        setNavOpen(!navOpen);
+    }
+    const[navOpen, setNavOpen] = useState("false");
   return (
     <div className='nav-container'>
         <div className='nav-logo'>
             <h1>Laviz</h1>
         </div>
-        <div className='nav-list-container'>
+        <div className={navOpen? 'nav-list-container': 'not-visible'}>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/projects">Works</Link></li>
                 <li><Link to="/experience">Experience</Link></li>
+                <li><Link to="/experience"><button className='nav-resume'> Resume </button></Link></li>
+
 
             </ul>
         </div>
-        <div className='nav-resume'>
-            <button> Resume </button>
-        </div>
+        <button className='nav-toggle-button' onClick={openNav}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+
+        </button>
     </div>
   )
 }
