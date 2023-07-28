f0d574bcdb24cf89553d6f80feaab43810239008
Laviz-pand <pandeylaviz@gmail.com>
2023-07-28 21:59:27 +0545

    feat: landing Page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,62 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import Navbar from './Navbar'
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import Navbar from "./Navbar";
 
 const HeroSection = () => {
   return (
     <Box position="relative">
-        <Navbar/>
+      <Navbar />
+      <Box position="relative">
+        <Box
+          pos={["static", "static", "absolute", "absolute"]}
+          zIndex={2}
+          height={["auto", "auto", "600px", "800px"]}
+          display="flex"
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          <Image
+            src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"
+            width="700px"
+            height="600px"
+            objectFit="cover"
+          />
+        </Box>
+        <Box
+          width={["auto", "auto", "600px", "800px"]}
+          bg="level1"
+          pos={["static", "static", "absolute", "absolute"]}
+          right={0}
+          height={["auto", "auto", "800px"]}
+          display="flex"
+          alignItems="center"
+          justifyContent={["center", "center", "center", "flex-end"]}
+          padding="48px 20px"
+        >
+          <Box zIndex={5} fontSize={["24px", "36px", "48px", "76px"]}>
+            <Text fontWeight="700" lineHeight="normal">
+              I'm Laviz Pandey
+            </Text>
+            <Text fontWeight="700" lineHeight="normal">
+              A software developer
+            </Text>
+            <Text fontWeight="700" color="darkGrey" lineHeight="normal">
+              based in Nepal
+            </Text>
+            <Text
+              fontSize={["8px", "12px", "16px", "18px"]}
+              fontWeight="400"
+              marginTop="32px"
+            >
+              I'm probably the most passionate designer you will ever get to
+              work with. If you have a great project that needs some amazing
+              skills, I'm your guy.
+            </Text>
+          </Box>
+        </Box>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,135 @@
-import { Box,Text } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import MenuIcon from './icons/MenuIcon'
-import { useMediaQuery } from '@chakra-ui/react'
+import { Box, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import MenuIcon from "./icons/MenuIcon";
+import { useMediaQuery } from "@chakra-ui/react";
 const Navbar = () => {
-    const [isMobile] = useMediaQuery("(max-width: 768px)")
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
-    <Box position="absolute" top={0} display="flex" flexDir="column" alignItems="center" justifyContent="space-between" paddingX="72px"  width="100%" >
-    <Box  display="flex" alignItems="center" justifyContent="space-between"  width="100%" >
-        
+    <Box
+      position="absolute"
+      top={0}
+      display="flex"
+      flexDir="column"
+      alignItems="center"
+      justifyContent="space-between"
+      paddingX="72px"
+      width="100%"
+      zIndex={10}
+    >
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+      >
         <Box>LOGO</Box>
-        { isMobile? <Box paddingY="24px">
-            <MenuIcon height={20} width={20}/>
-        </Box>:
-        <Box display="flex" justifyContent="center">
-            <Box display="flex" justifyContent="center" alignItems="center" padding="32px 24px">
-                <Link to="/experience">
-                    <Text>Experience</Text>
-                </Link>
+        {isMobile ? (
+          <Box
+            paddingY="24px"
+            onClick={() => setMenuOpen((item) => !item)}
+            cursor={"pointer"}
+          >
+            <MenuIcon height={20} width={20} />
+          </Box>
+        ) : (
+          <Box display="flex" justifyContent="center">
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              padding="32px 24px"
+            >
+              <Link to="/experience">
+                <Text>Experience</Text>
+              </Link>
             </Box>
-            <Box display="flex" justifyContent="center" alignItems="center" padding="32px 24px">
-                <Link to="/experience">
-                    <Text>Work</Text>
-                </Link>
-            </Box><Box display="flex" justifyContent="center" alignItems="center" padding="32px 24px">
-                <Link to="/experience">
-                    <Text>Photography</Text>
-                </Link>
-            </Box><Box display="flex" justifyContent="center" alignItems="center" padding="32px 24px" bg="primary">
-                <Link to="/experience">
-                    <Text>Contact</Text>
-                </Link>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              padding="32px 24px"
+            >
+              <Link to="/experience">
+                <Text>Work</Text>
+              </Link>
             </Box>
-
-
-
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              padding="32px 24px"
+            >
+              <Link to="/experience">
+                <Text>Photography</Text>
+              </Link>
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              padding="32px 24px"
+              bg="primary"
+            >
+              <Link to="/experience">
+                <Text>Contact</Text>
+              </Link>
+            </Box>
+          </Box>
+        )}
+      </Box>
+      {isMobile && isMenuOpen && (
+        <Box display="flex" flexDir="column" width="100%">
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            padding="12px 24px"
+            _hover={{ bg: "gray" }}
+          >
+            <Link to="/experience">
+              <Text>Experience</Text>
+            </Link>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            padding="12px 24px"
+            _hover={{ bg: "gray" }}
+          >
+            <Link to="/experience">
+              <Text>Work</Text>
+            </Link>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            padding="12px 24px"
+            _hover={{ bg: "gray" }}
+          >
+            <Link to="/experience">
+              <Text>Photography</Text>
+            </Link>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            padding="12px 24px"
+            bg="primary"
+            _hover={{ bg: "gray" }}
+          >
+            <Link to="/experience">
+              <Text>Contact</Text>
+            </Link>
+          </Box>
         </Box>
-        }
-
-</Box>
-{isMobile && 
-    <Box display="flex" flexDir="column">
+      )}
     </Box>
-    }
-    </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
